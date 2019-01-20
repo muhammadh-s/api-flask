9142a2903d33f99e6baa89cba5701e177ecca14f
Muhammad <muhammadhs@pm.me>
2019-01-20 22:46:18 +0500

    textbox added

--- front/src/components/InsertBox.js
+++ front/src/components/InsertBox.js
@@ -3,18 +3,16 @@ import React from 'react';
 const InsertBox = ({ value, handleChange }) => {
   return (
     <div className='pa2'>
-      <input
+      <textarea
         className='tc mt2 pa3 ba b--black bg-near-white'
         type='text'
         placeholder='Write a to-do here'
         onChange={handleChange}
-        size= '35'
+        rows="1"
+        cols="35"
         value = { value }
-
-
       />
-      {/* <textarea autofocus rows="4" cols="35"
-      /> */}
+
     </div>
   );
 }
