7291421f1a496edb51c14f439a2d957d43141d60
Muhammad <muhammadhs@pm.me>
2019-01-23 22:33:50 +0500

    added a font, adding PUT method

--- front/src/components/Card.js
+++ front/src/components/Card.js
@@ -3,7 +3,9 @@ import delIcon from '../delete.svg';
 
 const Card = ({ task, color, del, id }) => {
   return (
-    <div className={" card mw5 grow br2 ma3 dib ph4 bw2 pb3 pt3 shadow-1 " + "bg-" + color } >
+    <div
+      contenteditable="true"
+      className={" f5 card mw5 grow br2 ma3 dib ph4 bw2 pb3 pt3 shadow-1 " + "bg-" + color } >
         <img
           className = 'icon'
           src={delIcon}
@@ -12,6 +14,7 @@ const Card = ({ task, color, del, id }) => {
           onClick = { del }
         />
         <p>{ task }</p>
+        <button>Done</button>
     </div>
   );
 }
